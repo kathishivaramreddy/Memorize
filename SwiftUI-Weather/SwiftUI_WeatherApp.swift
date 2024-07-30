402efb2347f30d7d0e8013eecfb9643fc94fe3eb
Siva Ram Reddy Kathi <kathishivaram@gmail.com>
2024-07-30 12:06:20 -0500

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Shiva on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
