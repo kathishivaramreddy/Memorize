8992dd79eacfe9f213e698e4fe7d9b7305dd4578
Siva Ram Reddy Kathi <kathishivaram@gmail.com>
2024-11-22 20:29:11 -0600

    Add scroll view and aspect ratio to cards

--- Swift UI /Memorize/Memorize/ContentView.swift
+++ Swift UI /Memorize/Memorize/ContentView.swift
@@ -9,18 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let emojis = ["🦖","🐙","🐥","🐻"]
+    let emojis = ["🦖","🐙","🐥","🐻", "🐣","🦅","🐌","🦋","🐗","🙉","🐽","🪿","🦉","🐜","🐏"]
+    @State private var cardCount = 4
     
     var body: some View {
         
+        VStack {
+            ScrollView {
+                cards
+            }
+            
+              
+            Spacer()
+            
+            HStack {
+                cardCountAdjuster(by: -1, text: "Remove Card")
+                Spacer()
+                cardCountAdjuster(by: 1, text: "Add Card")
+            }
+            
+            
+            
+        }
+    }
+    
+    
+    var cards: some View {
         HStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+                
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index])
+                        .aspectRatio( 2/3, contentMode: .fit)
+                    
+                }
             }
         }
         .foregroundColor(.orange)
         .padding()
-       
+    }
+    
+    func cardCountAdjuster(by offSet: Int, text: String) -> some View {
+        Button(action: {
+            cardCount += offSet
+        }, label: {
+            Text(text)
+        })
+        .frame(maxHeight: 40)
+        .background(.orange)
+        .cornerRadius(5)
+        .disabled(cardCount + offSet < 1 || cardCount + offSet > emojis.count)
+        
     }
 }
 
@@ -36,17 +75,18 @@ struct CardView: View {
     
     var body: some View {
         ZStack {
-           let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
-                base
-                     .foregroundColor(.white)
-                base
-                     .strokeBorder(lineWidth: 2)
-                 Text(content)
-            } else {
-                base
-            }
+            let base = RoundedRectangle(cornerRadius: 12)
            
+            Group {
+                base
+                    .foregroundColor(.white)
+                base
+                    .strokeBorder(lineWidth: 2)
+                Text(content)
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        
+            
         }.onTapGesture {
             isFaceUp.toggle()
         }
